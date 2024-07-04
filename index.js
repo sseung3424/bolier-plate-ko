9d6de3308e697a04c9e5aa7e83a992c35d61124d
csh3424 <csh3424@icloud.com>
2024-07-04 11:31:17 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://seunghoon:csh3424@bolierplate.6nz9kxc.mongodb.net/?retryWrites=true&w=majority&appName=bolierplate'
-).then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI)
+.then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+   
+    const user = new User(req.body)
+
+    user.save()
+    .then(userInfo => res.status(200).json({ success: true}))
+    .catch(err => res.json({ success: false, err }));
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
